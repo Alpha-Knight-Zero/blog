d8888da7e5dbc883620d8ca4b018d2f13db62692
Alpha-Knight_Zero <pushkaldkpandey333@gmail.com>
2022-07-28 13:13:02 +0530

    included scss and set backgroud image

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,14 +20,14 @@ const Home: NextPage = () => {
 			<div className='grid grid-cols-1 gap-12 lg:grid-cols-12'>
 				<div className='col-span-1 lg:col-span-8'>
 					{posts.map((post) => (
-						<div>
-							{post.title}
-							{post.author}
-						</div>
+						<PostCard post={post} key={post.title} />
 					))}
 				</div>
 				<div className='col-span-1 lg:col-span-4'>
-					<div className='relative lg:sticky top-8'></div>
+					<div className='relative lg:sticky top-8'>
+						<PostWidget />
+						<Categories />
+					</div>
 				</div>
 			</div>
 		</div>
